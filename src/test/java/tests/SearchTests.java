1e43f97f1d50081d27cb269e75852c1cb8e933c9
annaegorova <anna-khabarova2107@yandex.ru>
2024-05-05 19:32:46 +0300

    first commit

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.className;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.id;
+
+public class SearchTests extends TestBase {
+    @Test
+    void successfulSearchTest() {
+
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+
+        step("Verify content", () ->
+            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0)));
+    }
+}
